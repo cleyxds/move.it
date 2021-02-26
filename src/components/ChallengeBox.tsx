43aa2f680809b1c5fbd2d352e00380946558398e
cleyxds <cleysonn@hotmail.com.br>
2021-02-26 18:08:06 -0300

    ChallengeBox + start ContextAPI

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,50 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+  const contextData = useContext(ChallengeContext);
+  
+  console.log(contextData);
+
+  const hasActiveChallenge = true;
+  
+  return (
+    <div className={styles.challengeBoxContainer}>
+      { hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça uma caminhada de 3 minutos.</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+            >
+              Falhei
+            </button>
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up"/>
+            Avance de level completando desafios
+          </p>
+        </div>
+      )}
+    </div>
+  )
+}
